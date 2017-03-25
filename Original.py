13a50b3a7253156bc6c71ca885147fe97926dae9
Bobbdee <youcanneverbetoocareful@gmail.com>
2017-03-25 00:27:14 -0400

    Tried to fix it

--- Original.py
+++ Original.py
@@ -1,28 +1,22 @@
 print("Sa-k passe Boss?!")
-g = input("Guess the number:")
-guess = int(g)
+r = input("Guess the number:")
+guess = int(r)
 if guess == 5:
   print("You got it!")
 else:
   while guess > 5: 
 #I changed the "if" to "while". When I ran the program, it looped, but I had to kill the program to make it stop.I guess that is progress because it is looping.
     print("That is too darn high")
-    g = input("Guess the number:") #Could it have previouly gone on an infinite loop because I didn't tell it what to do afterward? We shall see.
+    r = input("Guess the number:") #Could it have previously gone on an infinite loop because I didn't tell it what to do afterward? We shall see.
   else:
     while guess < 5:
       print("A shame, that's too low")
-      g = input("Guess the number:")
+      r = input("Guess the number:")
 print("The game is terminated!")
 #How do I activate syntax highlighting here?
 #Answer: type in .py at the end of the file's name.
 '''
-#This code is from p.29 in "Head First Programming"
-answer = "no"
-while answer == "no":
-  answer = input("Are we there?")
-print("We're there!")
-
-#This next code is woriking code from Khan Academy
+#This code is a working code from Khan Academy
 a = input("Guess my age: ")
 while a > "26":
     print("Nope! Too high!")
@@ -31,6 +25,14 @@ while a > "26":
         print ("Too low!")
         a = input("Guess my age: ")
 print("You rock!")
+
+#This code is from p.29 in "Head First Programming"
+answer = "no"
+while answer == "no":
+  answer = input("Are we there?")
+print("We're there!")
+
+*********************************************************************
 
 When I ran:
 print("Sa-k passe Boss?!")
