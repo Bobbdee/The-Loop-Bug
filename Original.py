44bbab4e1f26cb02f38fea166109c0798d9d7044
Bobbdee <youcanneverbetoocareful@gmail.com>
2017-05-20 23:16:58 -0400

    Update Original.py

--- Original.py
+++ Original.py
@@ -1,12 +1,12 @@
 print("Sa-k passe Boss?!")
 guess = 0
-while guess != 5:
-  g = input("Guess the numbe, Boss!")
-  guess = int(r)
-  if guess == 5:
+while guess != 56:
+  u = input("Guess the numbe, Boss!")
+  guess = int(u)
+  if guess == 56:
     print("You got it!")
   else:
-    if guess > 5:
+    if guess > 56:
       print("Too high, man!")
     else:
       print("Too low, man!")
